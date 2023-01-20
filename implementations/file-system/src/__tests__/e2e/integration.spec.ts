292875b35052df5db0519391b938053246979c85
krisbitney <kristoferbitney@hotmail.com>
2023-01-20 17:00:38 +0530

    updated fs resolver polywrap package versions and tests

--- implementations/file-system/src/__tests__/e2e/integration.spec.ts
+++ implementations/file-system/src/__tests__/e2e/integration.spec.ts
@@ -1,47 +1,20 @@
-import {
-  PolywrapClient,
-  UriResolverAggregator,
-  StaticResolver,
-  ExtendableUriResolver
-} from "@polywrap/client-js";
-import { WasmPackage } from "@polywrap/wasm-js";
-import path from "path";
+import { PolywrapClient } from "@polywrap/client-js";
 import fs from "fs";
+import {fsResolverUri, getClientConfig} from "../config";
 
-jest.setTimeout(60000);
+jest.setTimeout(120000);
 
 describe("file-system-uri-resolver-ext e2e tests", () => {
 
-  const dirname: string = path.resolve(__dirname);
-  const wrapperPath: string = path.join(dirname, "..", "..", "../build");
-  const wrapperPackage = WasmPackage.from(
-    fs.readFileSync(path.join(wrapperPath, "wrap.info")),
-    fs.readFileSync(path.join(wrapperPath, "wrap.wasm"))
-  );
-
-  const client = new PolywrapClient({
-    resolver: new UriResolverAggregator([
-      StaticResolver.from([
-        {
-          uri: "wrap://ens/wrappers.polywrap.eth:file-system-uri-resolver-ext@1.0.0",
-          package: wrapperPackage
-        }
-      ]),
-      // TODO: accept custom URI here + update URI to be new URI
-      new ExtendableUriResolver()
-    ]),
-    interfaces: [{
-      interface: ""
-    }]
-  }, { noDefaults: true })
+  const client = new PolywrapClient(getClientConfig(), { noDefaults: true })
 
   const manifest = fs.readFileSync(
     __dirname + "/../test-wrapper/wrap.info"
-  ).buffer;
+  ).buffer
 
   it("sanity - fs", async () => {
     const result = await client.invoke({
-      uri: wrapperUri,
+      uri: fsResolverUri,
       method: "tryResolveUri",
       args: {
         authority: "fs",
@@ -60,7 +33,7 @@ describe("file-system-uri-resolver-ext e2e tests", () => {
 
   it("sanity - file", async () => {
     const result = await client.invoke({
-      uri: wrapperUri,
+      uri: fsResolverUri,
       method: "tryResolveUri",
       args: {
         authority: "file",
@@ -79,7 +52,7 @@ describe("file-system-uri-resolver-ext e2e tests", () => {
 
   it("incorrect authority", async () => {
     const result = await client.invoke({
-      uri: wrapperUri,
+      uri: fsResolverUri,
       method: "tryResolveUri",
       args: {
         authority: "foo",
@@ -95,7 +68,7 @@ describe("file-system-uri-resolver-ext e2e tests", () => {
 
   it("found nothing", async () => {
     const result = await client.invoke({
-      uri: wrapperUri,
+      uri: fsResolverUri,
       method: "tryResolveUri",
       args: {
         authority: "file",
@@ -114,16 +87,14 @@ describe("file-system-uri-resolver-ext e2e tests", () => {
 
   it("getFile", async () => {
     const result = await client.invoke({
-      uri: wrapperUri,
+      uri: fsResolverUri,
       method: "getFile",
       args: {
         path: "./src/__tests__/test-wrapper/wrap.info"
       }
     });
 
-    expect(result.ok).toBeTruthy();
-    if (result.ok) {
-      expect(result.value).toMatchObject(manifest);
-    }
+    if (!result.ok) fail(result.error);
+    expect(result.value).toMatchObject(manifest);
   });
 });
