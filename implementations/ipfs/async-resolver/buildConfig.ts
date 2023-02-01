ae4bc04c6952280317920cab90ed87c41434cabe
krisbitney <kristoferbitney@hotmail.com>
2023-02-01 13:36:51 +0530

    updated uri for ipfs http client in async ipfs resolver wrapper

--- implementations/ipfs/async-resolver/buildConfig.ts
+++ implementations/ipfs/async-resolver/buildConfig.ts
@@ -4,7 +4,7 @@ export function configure(builder: IClientConfigBuilder): IClientConfigBuilder {
   return builder
     .addDefaults()
     .addRedirect(
-      "wrap://ens/ipfs-http-client.polywrap.eth",
+      "wrap://ens/wrappers.polywrap.eth:ipfs-http-client@1.0.0",
       "wrap://http/https://raw.githubusercontent.com/polywrap/ipfs/main/http-client/ipfs-http-client/build",
       )
 }
