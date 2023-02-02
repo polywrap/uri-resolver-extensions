b2f1455d71574569c51d6f3a167ec99cf970ed65
krisbitney <kristoferbitney@hotmail.com>
2023-02-02 18:10:57 +0530

    changed ipfs http client uri used in async ipfs uri resolver extension wrapper again, and removed buildConfig.ts

--- implementations/ipfs/async-resolver/src/__tests__/utils/config.ts
+++ implementations/ipfs/async-resolver/src/__tests__/utils/config.ts
@@ -31,7 +31,7 @@ export function getClientConfig(
           to: new Uri(ipfsResolverFsUri),
         },
         {
-          from: new Uri("wrap://package/ipfs-http-client"),
+          from: new Uri("wrap://ens/wrappers.polywrap.eth:ipfs-http-client@1.0.0"),
           to: new Uri("wrap://http/https://raw.githubusercontent.com/polywrap/ipfs/main/http-client/ipfs-http-client/build"),
         },
       {
