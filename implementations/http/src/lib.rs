1caf14e99960e4dc86847035013784f722d4a604
Jure Bogunovic <bogunovij@gmail.com>
2023-06-01 15:10:57 +0200

    updated http resolver to handle redirects if returned

--- implementations/http/src/lib.rs
+++ implementations/http/src/lib.rs
@@ -1,8 +1,10 @@
 pub mod wrap;
-use wrap::{*, imported::{ArgsGet}};
+use polywrap_wasm_rs::Map;
 use base64::decode;
+use wrap::{*, imported::ArgsGet};
 
 const MANIFEST_SEARCH_PATTERN: &str = "wrap.info";
+const URI_HEADER_KEY: &str = "x-wrap-uri";
 
 pub fn try_resolve_uri(args: ArgsTryResolveUri, _env: Option<Env>) -> Option<UriResolverMaybeUriOrManifest> {
     if args.authority != "http" && args.authority != "https" {
@@ -49,18 +51,37 @@ pub fn try_resolve_uri(args: ArgsTryResolveUri, _env: Option<Env>) -> Option<Uri
         Some(x) => x,
     };
 
+    let redirect_uri = get_redirect_uri_from_headers(&response.headers);
+
     match response.body {
-        None => return None,
-        Some(body) => match decode(body) {
-            Ok(body) => return Some(UriResolverMaybeUriOrManifest {
-                uri: None,
-                manifest: Some(body)
-            }),
-            Err(err) => {
-                panic!("Error during base64 decoding of body: {}", err.to_string());
+        None => None,
+        Some(body) => if body.len() == 0 {
+            Some(UriResolverMaybeUriOrManifest {
+                uri: redirect_uri,
+                manifest: None
+            })
+        } else {
+            match decode(body) {
+                Ok(body) => Some(UriResolverMaybeUriOrManifest {
+                    uri: redirect_uri,
+                    manifest: Some(body)
+                }),
+                Err(err) => {
+                    panic!("Error during base64 decoding of body: {}", err.to_string());
+                }
             }
         }
-    };
+    }
+}
+
+fn get_redirect_uri_from_headers(headers: &Option<Map<String, String>>) -> Option<String> {
+    if let Some(headers) = headers {
+        if let Some(uri) = headers.get(URI_HEADER_KEY) {
+            return Some(uri.clone())
+        }
+    }
+
+    None
 }
 
 pub fn get_file(args: ArgsGetFile, _env: Option<Env>) -> Option<Vec<u8>> {
