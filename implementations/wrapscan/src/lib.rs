7001de34526b740727444390f7b5b81b49e0085a
Pileks <jure@amber-it.co>
2023-07-05 17:24:39 +0200

    minor refactor

--- implementations/wrapscan/src/lib.rs
+++ implementations/wrapscan/src/lib.rs
@@ -3,20 +3,20 @@ pub use wrap::*;
 
 const DEFAULT_PROVIDER_URL: &str = "http/wraps.wrapscan.io";
 const RESOLVE_PATH: &str = "/r/";
+const AUTHORITY: &str = "wrapscan";
 
 impl ModuleTrait for Module {
     fn try_resolve_uri(
         args: ArgsTryResolveUri,
         env: Option<Env>,
     ) -> Result<Option<UriResolverMaybeUriOrManifest>, String> {
-        if args.authority != "wrapscan" {
+        if args.authority != AUTHORITY {
             return Ok(None);
         }
 
-        let provider_url = match env {
-            Some(env) => env.provider_url.unwrap_or(DEFAULT_PROVIDER_URL.to_string()),
-            None => DEFAULT_PROVIDER_URL.to_string(),
-        };
+        let provider_url = env
+            .and_then(|x| x.provider_url)
+            .unwrap_or(DEFAULT_PROVIDER_URL.to_string());
 
         let wrap_url = provider_url + RESOLVE_PATH + &args.path;
 
