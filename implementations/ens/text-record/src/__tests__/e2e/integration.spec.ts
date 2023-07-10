a125f7e6d64e3aa510c673614e4707652317d383
Cesar <cesarbrazon10@gmail.com>
2023-07-10 10:38:49 +0200

    chore(ens/text-record): tests fixed

--- implementations/ens/text-record/src/__tests__/e2e/integration.spec.ts
+++ implementations/ens/text-record/src/__tests__/e2e/integration.spec.ts
@@ -1,5 +1,5 @@
 import { UriResolutionContext, Uri } from "@polywrap/core-js";
-import { PolywrapClient, ExtendableUriResolver, ClientConfigBuilder } from "@polywrap/client-js";
+import { PolywrapClient, ExtendableUriResolver, PolywrapClientConfigBuilder, IWrapPackage } from "@polywrap/client-js";
 import path from "path";
 import {ethereumProviderPlugin, Connections} from "ethereum-provider-js";
 
@@ -9,9 +9,10 @@ describe("ens-text-record-resolver e2e tests", () => {
 
   const ethereumProviderUri = "wrap://ens/wraps.eth:ethereum-provider@1.1.0";
 
-  const builder = new ClientConfigBuilder()
-  builder.addPackage(ethereumProviderUri, ethereumProviderPlugin({ connections: new Connections({ networks: {} })}))
-  const client = builder.build();
+  const builder = new PolywrapClientConfigBuilder()
+  builder.addDefaults().setPackage(ethereumProviderUri, ethereumProviderPlugin({ connections: new Connections({ networks: {} })}) as IWrapPackage)
+
+  const client = new PolywrapClient(builder.build())
 
   let wrapperUri: string;
 
@@ -121,25 +122,8 @@ describe("ens-text-record-resolver e2e tests", () => {
   });
 
   it("recursively resolves", async () => {
-    const client = new PolywrapClient({
-      interfaces: [
-        {
-          interface: ExtendableUriResolver.extInterfaceUri.uri,
-          implementations: [wrapperUri]
-        },
-        {
-          interface: ethereumProviderUri,
-          implementations: [ethereumProviderUri]
-        }
-      ],
-      packages: [
-        {
-          uri: ethereumProviderUri,
-          package: ethereumProviderPlugin({ connections: new Connections({ networks: {} }) }),
-        }
-      ],
-    });
-
+    const builder = new PolywrapClientConfigBuilder()
+    const client = new PolywrapClient(builder.addDefaults().build())
     const result = await client.invoke({
       uri: wrapperUri,
       method: "tryResolveUri",
@@ -156,17 +140,15 @@ describe("ens-text-record-resolver e2e tests", () => {
         uri: "wrap://ens/goerli/wrappers.polywrap-test.eth:foo"
       });
     }
-
     const context = new UriResolutionContext();
     const uri = await client.tryResolveUri({
       uri: "ens/goerli/wrappers.polywrap-test.eth:package@1.0.0",
       resolutionContext: context
     });
     const path = context.getResolutionPath();
-
     expect(uri.ok).toBeTruthy();
     if (uri.ok) {
-      expect(uri.value.type).toBe("wrapper");
+      expect(uri.value.type).toBe("package");
     }
     expect(path).toStrictEqual([
       new Uri("ens/goerli/wrappers.polywrap-test.eth:package@1.0.0"),
